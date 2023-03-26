3859653b94370df13928e1d4fabead6148056963
ibrahimmt11 <abitmulyo@gmail.com>
2023-03-26 23:07:43 +0700

    implementasi design by contract

--- tpmodul6_1302210022/Program.cs
+++ tpmodul6_1302210022/Program.cs
@@ -7,7 +7,21 @@ class Program
     static void Main(string[] args)
     {
         SayaTubeVideo video1 = new SayaTubeVideo("Design by Contract - Ibrahim Mulyo Tsabit");
-        SayaTubeVideo video1 = new SayaTubeVideo(null);
-        SayaTubeVideo video1 = new SayaTubeVideo("Design by Contract");
+        SayaTubeVideo video2 = new SayaTubeVideo(null);
+        SayaTubeVideo video3 = new SayaTubeVideo("Design by Contract");
+
+        video1.IncreasePlayCount(500);
+        video2.IncreasePlayCount(15000000);
+        video3.IncreasePlayCount(-100);
+
+        Console.WriteLine("\n");
+
+        Console.WriteLine("Video detail:\n");
+        video1.PrintVideoDetail();
+        Console.WriteLine("\n");
+        video2.PrintVideoDetail();
+        Console.WriteLine("\n");
+        video3.PrintVideoDetail();
+        Console.WriteLine("\n");
     }
 }
